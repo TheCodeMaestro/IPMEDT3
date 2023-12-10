6eea078bcaac9e3025464982a209019a15030e01
TheCodeMaestro <129741847+TheCodeMaestro@users.noreply.github.com>
2023-12-10 19:50:55 +0100

    Controllers werken nu wel. Je kan nu dingen oppakken.

---
+++ main.js
@@ -0,0 +1,21 @@
+// window.onload = () =>{
+//     const places = document.getElementsByClassName('js--place');
+//     const camera = document.getElementById('js--camera');
+
+//     let pickups = document.getElementsByClassName('js--pickup');
+//     let hold = null;
+// }
+
+
+// window.onload = () =>{
+//     console.log("javascript file werkt");
+//     document.querySelector('a-box').addEventListener('click', function (evt) {
+//         console.log("Het werkt");
+//         var removeable = document.querySelector('a-box');
+//         removeable.parentNode.removeChild(removeable);
+//       });
+// // }
+
+// function remove(){
+    
+// }
