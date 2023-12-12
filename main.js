dec4cf7c665ca1c3c19b6f2ad65fbceedba79548
elise <s1141823@student.hsleiden.nl>
2023-12-12 13:41:46 +0100

    cubes

--- main.js
+++ main.js
@@ -19,3 +19,27 @@
 // function remove(){
     
 // }
+
+// x 	Negative X axis extends left. Positive X Axis extends right.
+// y 	Negative Y axis extends down. Positive Y Axis extends up.
+// z 	Negative Z axis extends in. Positive Z Axis extends out.
+
+window.onload = () => {
+  createBoxes();
+}
+
+function createBoxes() {
+  const scene = document.querySelector("a-scene");
+
+  for (let z = -5; z <= 5; z++) {
+    for (let y = 0; y >= -2; y--) {
+      for (let x = -5; x <= 5; x++) {
+        const cube = document.createElement("a-box");
+        cube.classList.add("js--cube", "js--clickable");
+        cube.setAttribute("position", `${x} ${y} ${z}`);
+        cube.setAttribute("rotation", "0 0 0");
+        scene.appendChild(cube);
+      }
+    }
+  }
+}
