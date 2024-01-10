b24d47e46287ce98e5b94b729d6b3388d1ca1e59
elise <s1141823@student.hsleiden.nl>
2024-01-10 09:34:16 +0100

    prototype

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// x 	Negative X axis extends left. Positive X Axis extends right.
+// y 	Negative Y axis extends down. Positive Y Axis extends up.
+// z 	Negative Z axis extends in. Positive Z Axis extends out.
+
 let camera;
 let numberOfEarthQuakes = 5;
 
@@ -5,14 +9,51 @@ window.onload = () => {
   camera = document.getElementById('js--camera');
   let sky = document.getElementById("background");
 
-  setTimeout(() => {
-    earthQuake();
-  }, 1200)
+  createBoxes();
+  randomizePositionFossils();
 
   setTimeout(() => {
-    sky.setAttribute("src", "#volcano-errupt");
-  }, 1000)
-};
+    earthQuake();
+  }, 3200)
+
+  setTimeout(() => {
+    sky.setAttribute("src", "#sky-errupt");
+  }, 3000)
+}
+
+function createBoxes() {
+  const scene = document.querySelector("a-scene");
+
+  for (let z = -5; z <= 5; z++) { // left right
+    for (let y = 0; y >= -2; y--) { // layers deep
+      for (let x = -5; x <= 5; x++) { // front behind
+        const cube = document.createElement("a-box");
+        cube.classList.add("js--cube", "js--clickable");
+        cube.setAttribute("position", `${x} ${y} ${z}`);
+        cube.setAttribute("color", "#daa46d")
+        scene.appendChild(cube);
+      }
+    }
+  }
+}
+
+function randomizePositionFossils() {
+  const fossils = document.getElementsByClassName("fossils");
+
+  for (let i = 0; i < fossils.length; i++) {
+    const fossilEl = fossils[i];
+    let fossilElID = fossilEl.getAttribute("gltf-model");
+
+    fossilsPosX = getRandomInt(-5, 6);
+    fossilsPosY = getRandomInt(-5, 6);
+    
+    if (fossilElID == "#fossil-1") {
+      fossilEl.setAttribute("position", `${fossilsPosX} 0.6 ${fossilsPosY}`);
+    } if (fossilElID == "#fossil-2") {
+      fossilEl.setAttribute("position", `${fossilsPosX} -0.4 ${fossilsPosY}`);
+    }
+  }
+}
 
 function earthQuake() {
   let i = 0;
@@ -48,4 +89,10 @@ function earthQuake() {
   }
 
   animate();
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min); //inclusive
+  max = Math.floor(max); //exclusive
+  return Math.floor(Math.random() * (max - min) + min);
 }
